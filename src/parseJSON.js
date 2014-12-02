07fc71858c2cb7541d8265e131fc35b0085c664a
Wayne Muller <github@wainage.com>
2014-12-01 22:02:31 -0500

    Created first attempt at parseJSON
    It's in a very dangerous state. Don't use it!!

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,8 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  // this is evil!!
+  // Please don't use this until you've fixed it. DEFINITELY don't let unsanitized code use this.
+  return eval('(' + json + ')');
 };
